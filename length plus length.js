a67989a3fd543facf94fef8db269e9aa995d0c6b
OlgaRomankova <olcha777@tut.by>
2024-03-23 17:32:05 +0300

    upd

--- length plus length.js
+++ length plus length.js
@@ -1,8 +1,7 @@
 
 function lengthCompany1PlusCompany1(value1,value2){
   return value1.length + value2.length
-  
-}
+  }
 
 console.log(lengthCompany1PlusCompany1('Apple','Samsung')) 
 console.log(lengthCompany1PlusCompany1('Olga','Romankova')) 
