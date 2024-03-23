3b10fd505920977f01fb385e6a886bf82ec435bc
OlgaRomankova <olcha777@tut.by>
2024-03-23 17:15:17 +0300

    + trash and length plus length

--- length plus length.js
+++ length plus length.js
@@ -7,3 +7,10 @@ function lengthCompany1PlusCompany1(value1,value2){
 
 console.log(lengthCompany1PlusCompany1('Apple','Samsung')) 
 console.log(lengthCompany1PlusCompany1('Olga','Romankova')) 
+
+const soldiersCount = -2309;
+
+console.log(Math.abs(soldiersCount))
+
+const number = 10.1234
+console.log(Math.round(number))
---
+++ trash.js
@@ -0,0 +1,5 @@
+const java = 'JavaScript'
+// Индексы начинаются с нуля
+// Вызов метода и вычитание вместе!
+const lastIndex = java.length - 1
+console.log(lastIndex) // 9
