bcf4dbe56983b382e60eb2f69237b663c63751cc
OlgaRomankova <olcha777@tut.by>
2024-03-27 21:47:15 +0300

    ex 1 upd

---
+++ Garina tasks third part.js
@@ -0,0 +1,18 @@
+// hello new branch
+// Returns a string that repeated the specified number of times.
+// @param {string} value
+// @param {string} count
+// @return {string}
+// @example
+// 'A', 5 => 'AAAAA'
+// 'cat', 3 => 'catcatcat'
+// */
+// function repeatString(value, count) {
+// return
+// }
+
+function repeatString(value, count) {
+    return value.repeat(count)
+    }
+    console.log(repeatString("A", 5))
+    console.log(repeatString("cat", 3))
