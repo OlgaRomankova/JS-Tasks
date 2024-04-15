fffb7447c5e9c4bdd993252c06e4ff8f25cf272f
OlgaRomankova <olcha777@tut.by>
2024-04-15 08:57:09 +0300

    revers each word in the string

---
+++ reverses each word in the string.js
@@ -0,0 +1,10 @@
+function reverseWords(str) {
+    return str.split("") // ("") разбивает на элементы
+    .reverse()           // переворачивает все предложение
+    .join("")            // ("") соединяет в слова
+    .split(" ")          // ("_") разбирает на слова
+    .reverse()           // переворачивает элементы с слове
+    .join(" ")           // ("_") соберет в слова ("") соберет все вместе
+};
+console.log(reverseWords("sihT si na !elpmaxe"))
+console.log(reverseWords("elbuod  secaps"))
